47f2031517f5d611304a301ce18ac743f6f6c1cd
Matias Bella <matias.bella@hotmail.com>
2023-10-03 18:47:38 -0300

    chapter 7 things

---
+++ ch7/restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,6 @@
+pub fn assing_seat() {
+    ()
+}
+pub fn add_to_waitlist() {
+    ()
+}
---
+++ ch7/restaurant/src/front_of_house/serving.rs
@@ -0,0 +1,9 @@
+pub fn take_order() {
+    ()
+}
+pub fn serve_order() {
+    ()
+}
+pub fn take_payment() {
+    ()
+}
